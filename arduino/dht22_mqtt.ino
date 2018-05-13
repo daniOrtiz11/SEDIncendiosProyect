bc2b5d590891a1514d0a10cdada7bf9ac184b670
amerayo <amerayo@ucm.es>
2018-05-13 17:50:10 +0200

    enciende el led con una humedad alta

--- arduino/dht22_mqtt.ino
+++ arduino/dht22_mqtt.ino
@@ -1,26 +1,21 @@
 /*
  Basic ESP8266 MQTT example
-
  This sketch demonstrates the capabilities of the pubsub library in combination
  with the ESP8266 board/library.
-
  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic" every two seconds
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
   - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
     else switch it off
-
  It will reconnect to the server if the connection is lost using a blocking
  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
  achieve the same result without blocking the main loop.
-
  To install the ESP8266 board, (using Arduino 1.6.4+):
   - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
        http://arduino.esp8266.com/stable/package_esp8266com_index.json
   - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
   - Select your ESP8266 in "Tools -> Board"
-
 */
 
 #include <ESP8266WiFi.h>
@@ -34,8 +29,8 @@
 // Update these with values suitable for your network.
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* ssid = "Moto G (5) Plus 8528";
-const char* password = "Primaswifi";
+const char* ssid = "PRIMA'S WIFI";
+const char* password = "Pr1m@s15072016";
 const char* mqtt_server = "test.mosquitto.org";
 
 WiFiClient espClient;
@@ -47,6 +42,7 @@ int value = 0;
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
+  digitalWrite(BUILTIN_LED, HIGH);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -85,10 +81,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
 
@@ -126,17 +118,13 @@ void loop() {
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
 
   // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
   // Compute heat index in Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
@@ -144,11 +132,9 @@ void loop() {
   client.publish("presence", msg);
   snprintf(msg, 75, "Temperatura ºC: %f", t);
   client.publish("presence", msg);
-  snprintf(msg, 75, "Temperatura *F: %f", f);
-  client.publish("presence", msg);
   snprintf(msg, 75, "Heat index ºC: %f", hic);
-  client.publish("presence", msg);
-  snprintf(msg, 75, "Heat index *F: %f", hif);
-  
-  
+
+  if(h>80){
+    digitalWrite(BUILTIN_LED, LOW);
+  }
 }
